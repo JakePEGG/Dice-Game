d05ae503f46056b659674f8974da566dbf79fca0
Jake Atkin <jakeatkin@Jakes-MacBook-Air.local>
2019-01-10 13:18:19 +0000

    Added dice roll values and created tests

--- dice_game/spec/dice_spec.rb
+++ dice_game/spec/dice_spec.rb
@@ -5,4 +5,9 @@ describe Dice do
   dice = Dice.new
   expect(dice).to respond_to(:roll)
 end
-end 
+
+ it "rolls a number between 1-6" do
+   dice = Dice.new
+   expect(dice.roll).to be_between(1, 6)
+ end
+end
